c593040d5922a5d348263debdc0d11cb423df489
Dharin S <dharinshah6989@gmail.com>
2015-12-25 17:16:50 +0530

    fixed indentation of cluster and node class

--- src/Nodes.java
+++ src/Nodes.java
@@ -2,13 +2,13 @@ import java.awt.Color;
 import java.awt.Graphics;
 
 public class Nodes {
-	static public final int shapeRadius = 2;
-	   Color color;
-	   int x;
-	   int y;
-	   void draw(Graphics g) {
-	      g.setColor(this.color);
-	      g.drawLine(this.x - shapeRadius, this.y, this.x + shapeRadius, this.y);
-	      g.drawLine(this.x, this.y - shapeRadius, this.x, this.y + shapeRadius);
-	    }
+static public final int shapeRadius = 2;
+   Color color;
+   int x;
+   int y;
+   void draw(Graphics g) {
+	  g.setColor(this.color);
+	  g.drawLine(this.x - shapeRadius, this.y, this.x + shapeRadius, this.y);
+	  g.drawLine(this.x, this.y - shapeRadius, this.x, this.y + shapeRadius);
 	}
+}
